2ea9cc775c0690eb436e00b02334200655c35970
Brandon Lin <brandonlin9716@gmail.com>
2024-07-01 18:49:48 -0700

    Implement function to play a round of rock-paper-scissors
    playRound() accepts two inputs: Human's choice and computer's choice.
    playRound() will determine the winner and increment the winners' score.

--- javascript.js
+++ javascript.js
@@ -18,3 +18,59 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    let losingMessage = `You lose! ${computerChoice} beats ${humanChoice}`;
+    let winningMessage = `You win! ${humanChoice} beats ${computerChoice}`;
+    let tieMessage = `You tied! You both picked ${humanChoice}`;
+    let returnMessage;
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                returnMessage = tieMessage;
+                break;
+            case "paper":
+                returnMessage = losingMessage;
+                computerScore++;
+                break;
+            default:
+                returnMessage = winningMessage;
+                humanScore++;
+        }
+    } else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                returnMessage = winningMessage;
+                humanScore++;
+                break;
+            case "paper":
+                returnMessage = tieMessage;
+                break;
+            default:
+                returnMessage = losingMessage;
+                computerScore++;
+        }
+    } else {
+        // humanChoice == "scissors" || human entered invalid text
+        switch (computerChoice) {
+            case "rock":
+                returnMessage = losingMessage;
+                computerScore++;
+                break;
+            case "paper":
+                returnMessage = winningMessage;
+                humanScore++;
+                break;
+            default:
+                returnMessage = tieMessage;
+        }
+    }
+    console.log(returnMessage);
+}
+
+// execution section
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
